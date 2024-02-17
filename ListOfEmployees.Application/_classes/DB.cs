76ec35d43009aa40d75a70501a04e1ab7a5cbd5a
TihonchukArtyom89 <mortus1989@gmail.com>
2024-02-17 19:30:32 +0300

    начал работать над классом DB, который служит для обработки базы данных (сделал метод получения таблицы)

---
+++ ListOfEmployees.Application/_classes/DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;//local DB0
+using System.Data;//ConnectionSate, DataTable
+
+namespace ListOfEmployees.Application._classes
+{
+    public static class DB
+    {
+        public static SqlConnection GetDBConnection()
+        {
+
+        }
+        public static DataTable GetDataTable()
+        {
+            SqlConnection connection = GetDBConnection();
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+}
+
+
